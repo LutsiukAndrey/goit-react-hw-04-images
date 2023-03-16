755b4617335f7021de35b7410d4d5fcc5f360706
Andrii <lutsiukandrey1@gmail.com>
2023-03-16 17:41:59 +0200

    fimish modal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -24,6 +24,7 @@ export const ImageGallery = ({ photoQuery }) => {
         setLoading(true);
       }
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [photoQuery]);
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import s from './Modal.module.css';
 import PropTypes from 'prop-types';
 
-export class Modal extends React.Component {
-  handleEscKey = e => {
-    if (e.code !== 'Escape') {
-      return;
-    }
-    this.props.toggleModal();
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscKey);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscKey);
-  }
+export const Modal = ({ modaiImg, toggleModal }) => {
+  const overlay = useRef();
 
-  render() {
-    return (
-      <div
-        className={s.backdrop}
-        onClick={e => {
-          if (e.target.nodeName !== 'DIV') {
-            return;
-          }
-          this.props.toggleModal(e);
-        }}
-      >
-        <div className={s.Modal}>
-          <img src={this.props.modaiImg} alt="" />
-        </div>
+  useEffect(() => {
+    overlay.current.focus();
+  }, []);
+
+  return (
+    <div
+      ref={overlay}
+      tabIndex={-1}
+      onKeyDown={event => {
+        if (event.code !== 'Escape') {
+          return;
+        }
+        toggleModal();
+      }}
+      className={s.backdrop}
+      onClick={e => {
+        if (e.target.nodeName !== 'DIV') {
+          return;
+        }
+        toggleModal(e);
+      }}
+    >
+      <div className={s.Modal}>
+        <img src={modaiImg} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   modaiImg: PropTypes.string,
